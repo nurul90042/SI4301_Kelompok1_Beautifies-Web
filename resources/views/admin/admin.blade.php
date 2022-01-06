d37de4cf8b87d2f6746c4f2fabbcb66e3663c8c8
nurul90042 <wahdanurul.nw@gmail.com>
2022-01-07 02:25:29 +0700

    admin-weh

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -282,11 +282,11 @@
 
 
                 <div class="row">
-                    <div class="col-md-12">
+                    {{-- <div class="col-md-12">
                         <h1 class="page-header">
                             Dashboard <small>Summary of your App</small>
                         </h1>
-                    </div>
+                    </div> --}}
                 </div>
                 @yield('main')
                 <!-- /. ROW  -->
