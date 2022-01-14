e27c1e15b3c569aa5eaec07820e1a7bb86e27dff
DarmaP135 <darma.wijaya1010@gmail.com>
2022-01-14 15:13:33 +0700

    solve sepertinya

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -21,7 +22,7 @@ class RegisterController extends Controller
         $add->last_name = $request->customerlast_name;
         $add->user_name = $request->customeruser_name;
         $add->email = $request->customeremail;
-        $add->password = $request->customerpassword;
+        $add->password = Hash::make($request->customerpassword);
 
         $add->save();
 
