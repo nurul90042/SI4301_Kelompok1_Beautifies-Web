862a129d73cc7242426fcf8a583409a936f8c9b9
DarmaP135 <darma.wijaya1010@gmail.com>
2022-01-14 11:23:39 +0700

    add user baru setengah

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -11,5 +11,18 @@ class RegisterController extends Controller
      
         return view('register');
         
+    } 
+
+    public function store(Request $request)
+    {
+        $add = new User();
+
+        $add->name = $request->customerfirst_name;
+        $add->email = $request->customeremail;
+        $add->password = $request->customerpassword;
+
+        $add->save();
+
+        return redirect(route('login'));
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,30 +15,31 @@
         	<div class="row">
                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 main-col offset-md-3">
                 	<div class="mb-4">
-                       <form method="post" action="#" id="CustomerLoginForm" accept-charset="UTF-8" class="contact-form">	
+                       <form  action="/register1" method="post"  id="CustomerLoginForm" accept-charset="UTF-8" class="contact-form">	
+                           @csrf
                           <div class="row">
 	                          <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                                 <div class="form-group">
                                     <label for="FirstName">First Name</label>
-                                    <input type="text" name="customer[first_name]" placeholder="" id="FirstName" autofocus="">
+                                    <input type="text" name="customerfirst_name" placeholder="" id="FirstName" autofocus="">
                                 </div>
                                </div>
                                <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                                 <div class="form-group">
                                     <label for="LastName">Last Name</label>
-                                    <input type="text" name="customer[last_name]" placeholder="" id="LastName">
+                                    <input type="text" name="customerlast_name" placeholder="" id="LastName">
                                 </div>
                                </div>
                             <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                                 <div class="form-group">
                                     <label for="CustomerEmail">Email</label>
-                                    <input type="email" name="customer[email]" placeholder="" id="CustomerEmail" class="" autocorrect="off" autocapitalize="off" autofocus="">
+                                    <input type="email" name="customeremail" placeholder="" id="CustomerEmail" class="" autocorrect="off" autocapitalize="off" autofocus="">
                                 </div>
                             </div>
                             <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                                 <div class="form-group">
                                     <label for="CustomerPassword">Password</label>
-                                    <input type="password" value="" name="customer[password]" placeholder="" id="CustomerPassword" class="">                        	
+                                    <input type="password" value="" name="customerpassword" placeholder="" id="CustomerPassword" class="">                        	
                                 </div>
                             </div>
                           </div>
